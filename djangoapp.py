d18391b10a89e281fe81948179edf7e0570f4fc2
jcallin <j.callin@hotmail.com>
2016-04-16 19:37:46 -0700

    modified test

--- djangoapp.py
+++ djangoapp.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-  """Respond to incoming calls with a simple text message."""
+    """Respond to incoming calls with a simple text message."""
 
-   resp = twilio.twiml.Response()
-   resp.message("Hello, Mobile Monkey")
-   return str(resp)
+    resp = twilio.twiml.Response()
+    resp.message("Hello, Mobile Monkey")
+    return str(resp)
 
 if __name__ == "__main__":
-   port = int( os.environ.get( "PORT",5000 ) )           
-   app.run(host="0.0.0.0", port=port)
+    port = int( os.environ.get( "PORT",5000 ) )           
+    app.run(host="0.0.0.0", port=port)
