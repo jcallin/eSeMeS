605ed189e5cf3b6496d931207500083bc3cd82c5
jcallin <j.callin@hotmail.com>
2016-04-17 01:01:48 -0700

    wiki stuffs

--- wiki.py
+++ wiki.py
@@ -2,14 +2,15 @@ import wikipedia
 import string
 
 def searchWiki( command ):
-    words = command.split()
-    word = words[1]
+    keyword = command.split()[0]
+    words = command.split(keyword)[1]
 
-    page = wikipedia.page( word )
+    page = wikipedia.page( words )
     content = page.summary
     content = filter(lambda x: x in string.printable, content )
 
     response=""
     for i in content:
         response += i
+    print( response )
     return( response )
