9f88696ce800c8315584f5753a6785d02577280c
jcallin <j.callin@hotmail.com>
2016-04-16 19:06:30 -0700

    working djangoapp

--- djangoapp.py
+++ djangoapp.py
@@ -1,38 +1,36 @@
 from flask import Flask, request, redirect
 import twilio.twiml
 from twilio.rest import TwilioRestClient
- 
+import os
+
 app = Flask(__name__)
 
-def send_message():
-    client.messages.create(
-        to=userNumber,
-        from_=OUR_NUMBER,
-        body=response,
-        media_url=media_response,
-    )
-
-
-#from Twilio acct
 ACCOUNT_SID = "ACe892e28b309a94844845c5f621ca451a"
 AUTH_TOKEN = "[99426e30a3c5c2e25d5ba56f9087b7c3]"
 OUR_NUMBER = "+18442306122"
 
-#needs to be a string
-userNumber= request.values.get('From', None)
+
+client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 
 @app.route("/", methods=['GET', 'POST'])
-client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
-while true:
-    clientMessages = client.messages.list( to=OUR_NUMBER )
+
+def send_message():
+    client.messages.create(
+            to=userNumber,
+            from_ = OUR_NUMBER,
+            body = response,
+            media_url = media_response
+            )
+
+while True:
+    clientMessages = client.messages.list(to=OUR_NUMBER)
     for m in clientMessages:
         if m.status == "receiving":
-            #do the things
-            userNumber=m.From
-            response= m.status
+            userNumber = m.From
+            response = m.status
             send_message()
         if m.status == "undelivered" or m.status == "failed":
-            #TBD
+            response = "died"
 
 response = "4321"
 media_response = ""
@@ -40,5 +38,6 @@ media_response = ""
 send_message()
 
 if __name__ == "__main__":
-    port = int( os.environ.get( "PORT",5000 ) )           
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
