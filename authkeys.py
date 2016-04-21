2020b1a7253dde2fade82fb9203f62bf44278761
jcallin <j.callin@hotmail.com>
2016-04-20 23:21:19 -0700

    started authkeys file

---
+++ authkeys.py
@@ -0,0 +1,13 @@
+def AuthKeys():
+    def Twilio():
+        ACCOUNT_SID = "ACe892e28b309a94844845c5f621ca451a"
+        AUTH_TOKEN = "99426e30a3c5c2e25d5ba56f9087b7c3"
+        OUR_NUMBER = "+18442306122"
+    def Google():
+        MAPS_API_KEY = "AIzaSyAPBGZ_K6NGbWnCup_lLO8Tk1fR1m_M2SU"
+    def Yelp():
+        CONSUMER_KEY = "Ayd6nwKZL5R2GvPdHo3nDA"
+        CONSUMER_SECRET = "gqFNLXXFT9UPq1tYmNPsu-bfYoQ"
+        TOKEN = "B-_C5-Om-Wri9P2RB7pUa5ihY0YLMKtA"
+        TOKEN_SECRET = "vozSr1-OTShWcvahAnRmvAFPXr8"
+
